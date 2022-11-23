b6bc580d78f0e655771d6f6fd4e6f7291267b2a8
navir-rahman <navirrahman@gmail.com>
2022-11-23 14:26:21 +0600

    expo video av added working version

--- App.js
+++ App.js
@@ -1,10 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { Video } from 'expo-av';
+import React from 'react';
 export default function App() {
+
+  const video = React.useRef(null);
+  const secondVideo = React.useRef(null)
+  const [status, setStatus ]= React.useState({});
+  const [statusSecondVideo, setStatusVideo] = React.useState({});
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Video 
+        ref={video}
+        style={styles.video}
+        source={{uri: "https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4"}}
+        useNativeControls
+        resizeMode="contain"
+        isLooping
+        onPlaybackStatusUpdate={setStatus}
+      />
+      <View style={styles.buttons}>
+        <Button title='play from 5s' onPress={()=>video.current.playFromPositionAsync(50000)} />
+        <Button title={status.isLooping ? "set to not loop" : "set to loop "} onPress={()=>video.current.setIsLoopingAsync(!status.isLooping)} />
+        <Button
+          title={status.isPlaying ? 'Pause' : 'Play'}
+          onPress={() =>
+            status.isPlaying ? video.current.pauseAsync() : video.current.playAsync()
+          }
+        />
+      </View>
+      <Video 
+        ref={secondVideo}
+        style={styles.video}
+        source={{uri: "https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4"}}
+        resizeMode="contain"
+        isLooping
+        onPlaybackStatusUpdate={setStatusVideo}
+      />
+      <View style={styles.buttons}>
+        <Button title='play from 5s' onPress={()=>secondVideo.current.playFromPositionAsync(50000)} />
+        <Button title={statusSecondVideo.isLooping ? "set to not loop" : "set to loop "} onPress={()=>secondVideo.current.setIsLoopingAsync(!status.isLooping)} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +55,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  video :{
+    flex:1,
+    alignSelf: 'stretch'
+  },
+  buttons:{
+    margin: 16
+  }
 });
