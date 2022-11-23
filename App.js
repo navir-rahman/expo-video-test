9d4232079abfac14b81b6f79cf71ca6c89621329
navir-rahman <navirrahman@gmail.com>
2022-11-23 15:32:06 +0600

    youtube added

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import Videos from './component/videos/Videos';
+import Youtubes from './component/Youtube/Youtubes';
 export default function App() {
 
 
@@ -9,8 +10,16 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <ScrollView>
+        {/* normal video  */}
       <Videos></Videos>
+      
+      {/* youtube video  */}
+
+      <Youtubes></Youtubes>
+      </ScrollView>
       <StatusBar style="auto" />
+
     </View>
   );
 }
